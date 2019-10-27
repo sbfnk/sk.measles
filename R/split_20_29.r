c1edbcc2b545dc28f899cb2156ebfbe1b9770f50
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2019-10-27 22:08:56 +0000

    add comments

--- R/split_20_29.r
+++ R/split_20_29.r
@@ -9,10 +9,12 @@ split_20_29 <- function(contacts)
   ## identify column
   twtw9 <- which(colnames(contacts)=="20-29")
 
+  ## duplicate row/column
   contacts_sa <-
     contacts[c(1:twtw9, twtw9:ncol(contacts)), c(1:twtw9, twtw9:ncol(contacts))]
   colnames(contacts_sa)[twtw9 + (0:1)] <- c("20-24", "25-29")
 
+  ## halve within-group contacts
   contacts_sa[, twtw9 + (0:1)] <- contacts_sa[, twtw9 + (0:1)] / 2
 
   off_diag <- unname(contacts[twtw9, twtw9+1] / 2)
